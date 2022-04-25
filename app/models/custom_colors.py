b001f9b6516127f57a3c2fa8ad7b5ff35deee721
paulina <paulina@kuczynska.cc>
2022-04-26 00:22:42 +0200

    manage xml

--- app/models/custom_colors.py
+++ app/models/custom_colors.py
@@ -10,22 +10,7 @@ def set_custom_colors(names, values):
     manage_xml.register_all_namespaces(file)
     tree = ET.parse(file)
     root = tree.getroot()
-
-    for element in root:
-        if element.tag == tag:
-            element.clear()
-    if root.find(tag) is None:
-        color_main_element = ET.Element(tag)
-        root.insert(3, color_main_element)
-
-    color_list = root.find(tag)
-    for n, v in zip(names, values):
-        color_name = ET.SubElement(color_list, 'a:custClr')
-        color_value = ET.SubElement(color_name, 'a:srgbClr')
-        color_name.set('name', n)
-        color_value.set('val', v)
-
-    tree.write(file,
-            xml_declaration=True,
-            encoding='utf-8',
-            method='xml')
+    manage_xml.remove_subtags(root, tag)
+    manage_xml.add_main_tag(root, tag, 3)
+    manage_xml.add_custom_color(root, tag, names, values)    
+    manage_xml.write_xml(tree, file)
--- app/models/manage_xml.py
+++ app/models/manage_xml.py
@@ -4,3 +4,29 @@ def register_all_namespaces(filename):
     namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
     for ns in namespaces:
         ET.register_namespace(ns, namespaces[ns])
+
+def remove_subtags(root, tag):
+    for element in root:
+        if element.tag == tag:
+            element.clear()
+
+def add_main_tag(root, tag, position):
+    if root.find(tag) is None:
+        color_main_element = ET.Element(tag)
+        root.insert(position, color_main_element)
+
+def add_custom_color(root, tag, name, value):
+    color_list = root.find(tag)
+    for n, v in zip(name, value):
+        color_name = ET.SubElement(color_list, 'a:custClr')
+        color_value = ET.SubElement(color_name, 'a:srgbClr')
+        color_name.set('name', n)
+        color_value.set('val', v)
+
+def write_xml(tree, filename):
+    tree.write(
+        filename,
+        xml_declaration=True,
+        encoding='utf-8',
+        method='xml'
+        )
